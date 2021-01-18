e12b92f8f101b00ba328c205479b92112a5e40bf
Jijo James <jijojames18@gmail.com>
2021-01-18 13:49:56 +0000

    Added Bootstrap vue and prettier cleanup

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
+import Vue from 'vue';
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue';
+import Axios from 'axios';
+
+import App from './App.vue';
+import router from './router';
+import './registerServiceWorker';
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+Vue.use(BootstrapVueIcons);
 
 Vue.config.productionTip = false;
+
+Axios.defaults.baseURL = process.env.VUE_APP_API_ENDPOINT_URL;
 
 new Vue({
   router,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
