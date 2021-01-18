c024a6f0f7136744a7396fb70ce082ec4c70b4eb
Jijo James <jijojames18@gmail.com>
2021-01-18 20:37:28 +0530

    Babel and eslint config

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
-  presets: ["@vue/cli-plugin-babel/preset"]
+  presets: [
+    [
+      '@babel/preset-env',
+      {
+        corejs: 3,
+        debug: false,
+        useBuiltIns: 'entry',
+        targets: {
+          node: 'current'
+        }
+      }
+    ],
+    ['@vue/app', { useBuiltIns: 'entry' }]
+  ]
 };
